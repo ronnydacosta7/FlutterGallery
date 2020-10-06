cc212dc4e65fa604da321fa315ec2389931b1720
Ronny Dacosta <RonnyWoren.Dacosta@mitrais.com>
2020-10-06 09:24:28 +0800

    add config

---
+++ src/test/java/Config.java
@@ -0,0 +1,34 @@
+import java.net.URL;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class Config {
+    public static AndroidDriver<MobileElement> driver;
+    @BeforeTest
+    public void setup(){
+        try {
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability("platformName", "Android");
+            caps.setCapability("deviceName", "emulator-5554");
+            caps.setCapability("automationName", "UiAutomator2");
+            caps.setCapability("appPackage", "io.flutter.demo.gallery");
+            caps.setCapability("appActivity", ".MainActivity");
+
+            URL url = new URL("http://127.0.0.1:4723/wd/hub");
+            driver = new AndroidDriver<MobileElement>(url,caps);
+            Thread.sleep(4000);
+        }catch(Exception exp) {
+            System.out.println("Cause is : " + exp.getCause());
+            System.out.println("Message is : " + exp.getMessage());
+            exp.printStackTrace();
+        }
+    }
+
+    @AfterTest
+    public void teardown(){
+        driver.quit();
+    }
+}
